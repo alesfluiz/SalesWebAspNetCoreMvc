9aa2fce27dd024fb53e1fb05494420a85bd60a4a
unknown <alesfluiz@gmail.com>
2023-06-03 22:48:04 -0300

    CRUD Scaffolding

---
+++ ProjectWebAspNet/Data/ProjectWebAspNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectWebAspNet.Models;
+
+namespace ProjectWebAspNet.Data
+{
+    public class ProjectWebAspNetContext : DbContext
+    {
+        public ProjectWebAspNetContext (DbContextOptions<ProjectWebAspNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectWebAspNet.Models.Department> Department { get; set; }
+    }
+}
