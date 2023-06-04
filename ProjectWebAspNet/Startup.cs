3ba225e51b67ec62d18f115738250444e5d59574
unknown <alesfluiz@gmail.com>
2023-06-03 23:39:42 -0300

    MySQL adaptation and first migration

--- ProjectWebAspNet/Startup.cs
+++ ProjectWebAspNet/Startup.cs
@@ -37,7 +37,8 @@ namespace ProjectWebAspNet
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjectWebAspNetContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjectWebAspNetContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ProjectWebAspNetContext"), builder=> 
+                    builder.MigrationsAssembly("ProjectWebAspNet")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
