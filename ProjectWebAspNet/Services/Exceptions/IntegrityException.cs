f94fdefac3f0d7f5887ceb38da670ba2ed0936f2
unknown <alesfluiz@gmail.com>
2023-06-11 15:59:36 -0300

    Exception handling for delete (referential integrity)

---
+++ ProjectWebAspNet/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace ProjectWebAspNet.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
--- ProjectWebAspNet/Services/SellerService.cs
+++ ProjectWebAspNet/Services/SellerService.cs
@@ -35,10 +35,17 @@ namespace ProjectWebAspNet.Services
         }
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
-        }
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
+            }
 
         public async Task UpadateAsync(Seller obj)
         {
